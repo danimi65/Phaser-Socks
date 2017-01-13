ed7b8b24ea0a18b903d4c13abfcb34fb4f4433ac
Danika Harada <danimi65@gmail.com>
2017-01-12 20:56:11 -1000

    OP

---
+++ public/js/OP.js
@@ -0,0 +1,52 @@
+//jshint esversion:6
+
+(function(){
+
+  /*
+   * Helper methods
+   */
+  const parse = message => {
+    let parsedMessage = JSON.parse(message);
+    if( !parsedMessage.hasOwnProperty('OP') ){
+      throw new Error('Improperly formatted OP message.');
+    }
+    return parsedMessage;
+  };
+
+  const create = (OP, payload) => JSON.stringify({
+    OP,
+    payload,
+  });
+
+  /*
+   * OP codes
+   */
+  const ERROR = 'ERROR';
+  const REGISTER = 'REGISTER';
+  const REGISTERBACK ='REGISTERBACK';
+
+  /*
+   * the module
+   */
+  const OP = {
+    create,
+    parse,
+    ERROR,
+    REGISTER,
+    REGISTERBACK,
+  };
+
+
+  /* Make this module available to Node and Browser */
+  const root = this;
+  if( typeof exports !== 'undefined' ) {
+    if( typeof module !== 'undefined' && module.exports ) {
+      exports = module.exports = OP;
+    }
+    exports.OP = OP;
+  }
+  else {
+    root.OP = OP;
+  }
+
+}).call(this);
